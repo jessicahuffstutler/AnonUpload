0daf3778bfba3e637ea5dbcafcf79d04e7eee391
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-11-18 11:58:26 -0500

    html, main.js, controller routes

---
+++ src/main/java/com/theironyard/controllers/AnonUploadController.java
@@ -0,0 +1,43 @@
+package com.theironyard.controllers;
+
+import com.theironyard.entities.AnonFile;
+import com.theironyard.services.AnonFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by jessicahuffstutler on 11/18/15.
+ */
+@RestController
+public class AnonUploadController {
+    @Autowired
+    AnonFileRepository files;
+
+    @RequestMapping("/files")
+    public List<AnonFile> getFiles() { //no git parameters needed
+        return (List<AnonFile>) files.findAll();
+    }
+
+    @RequestMapping("/upload")
+    public void upload(HttpServletResponse response, MultipartFile file) throws IOException { //we called it name="file" in html so we need MultipartFile file
+        File f = File.createTempFile("file", file.getOriginalFilename(), new File("public"));
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(file.getBytes());
+
+        AnonFile anonFile = new AnonFile();
+        anonFile.originalName = file.getOriginalFilename();
+        anonFile.name = f.getName();//name on the server, where we're saving it
+        files.save(anonFile);
+
+        response.sendRedirect("/");
+    }
+
+}
---
+++ src/main/java/com/theironyard/entities/AnonFile.java
@@ -0,0 +1,22 @@
+package com.theironyard.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by jessicahuffstutler on 11/18/15.
+ */
+@Entity
+@Table(name = "files")
+public class AnonFile {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+            //if this isnt public, it wont be send down in the json file
+    int id;
+
+    @Column(nullable = false)
+    public String originalName;
+
+    @Column(nullable = false)
+    public String name;
+}
